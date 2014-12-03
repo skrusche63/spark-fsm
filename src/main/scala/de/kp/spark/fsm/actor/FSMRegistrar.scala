ad75dce059314014f6f32e7300340ed58cc587da
skrusche63 <skrusche63@googlemail.com>
2014-12-03 07:10:44 +0100

    Functionality moved to 'spark-core'.

--- src/main/scala/de/kp/spark/fsm/actor/FSMRegistrar.scala
+++ src/main/scala/de/kp/spark/fsm/actor/FSMRegistrar.scala
@@ -18,44 +18,7 @@ package de.kp.spark.fsm.actor
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-import de.kp.spark.core.model._
-import de.kp.spark.fsm.model._
+import de.kp.spark.core.actor.ItemRegistrar
+import de.kp.spark.fsm.Configuration
 
-import scala.collection.mutable.ArrayBuffer
-
-class FSMRegistrar extends BaseActor {
-  
-  def receive = {
-    
-    case req:ServiceRequest => {
-      
-      val origin = sender    
-      val uid = req.data("uid")
-      
-      val response = try {
-        
-        /* Unpack fields from request and register in Redis instance */
-        val fields = ArrayBuffer.empty[Field]
-
-        fields += new Field("site","string",req.data("site"))
-        fields += new Field("timestamp","long",req.data("timestamp"))
-
-        fields += new Field("user","string",req.data("user"))
-        fields += new Field("group","string",req.data("group"))
-
-        fields += new Field("item","integer",req.data("item"))
-        cache.addFields(req, fields.toList)
-        
-        new ServiceResponse("series","register",Map("uid"-> uid),ResponseStatus.SUCCESS)
-        
-      } catch {
-        case throwable:Throwable => failure(req,throwable.getMessage)
-      }
-      
-      origin ! response
-
-    }
-    
-  }
-
-}
+class FSMRegistrar extends ItemRegistrar(Configuration)
