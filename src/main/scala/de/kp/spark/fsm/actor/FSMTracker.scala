f50ca0ae44ac16ad5c57af74e243d06fa994cf85
skrusche63 <skrusche63@googlemail.com>
2014-12-04 18:34:55 +0100

    Tracking functionality moved to 'spark-core'.

--- src/main/scala/de/kp/spark/fsm/actor/FSMTracker.scala
+++ src/main/scala/de/kp/spark/fsm/actor/FSMTracker.scala
@@ -18,109 +18,32 @@ package de.kp.spark.fsm.actor
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-import de.kp.spark.core.model._
 import de.kp.spark.core.Names
+import de.kp.spark.core.actor.BaseTracker
 
-import de.kp.spark.core.io.ElasticWriter
-
-import de.kp.spark.fsm.model._
+import de.kp.spark.fsm.Configuration
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.HashMap
 
-class FSMTracker extends BaseActor {
+class FSMTracker extends BaseTracker(Configuration) {
+  /*
+   * Example request data:
+   * 
+   * "uid": "123456"
+   * 
+   * "index": "orders"
+   * "type" : "products"
+   * 
+   * "site"    : "site-1"
+   * "user"    : "user-1"
+   * "timestamp: "1234567890"
+   * "group"   : "group-1"
+   * "item"    : "1,2,3,4,5,6,7"
+   * 
+   */    
   
-  def receive = {
-    /*
-     * Example request data:
-     * 
-     * "uid": "123456"
-     * 
-     * "index": "orders"
-     * "type" : "products"
-     * 
-     * "site"    : "site-1"
-     * "user"    : "user-1"
-     * "timestamp: "1234567890"
-     * "group"   : "group-1"
-     * "item"    : "1,2,3,4,5,6,7"
-     * 
-     */    
-    case req:ServiceRequest => {
-
-      val uid = req.data("uid")
-      
-      val data = Map("uid" -> uid, "message" -> Messages.TRACKED_ITEM_RECEIVED(uid))
-      val response = new ServiceResponse(req.service,req.task,data,ResponseStatus.SUCCESS)	
-      
-      val origin = sender
-      origin ! response
-
-      try {
-
-        val index   = req.data("index")
-        val mapping = req.data("type")
-    
-        val writer = new ElasticWriter()
-    
-        val readyToWrite = writer.open(index,mapping)
-        if (readyToWrite == false) {
-      
-          writer.close()
-      
-          val msg = String.format("""Opening index '%s' and mapping '%s' for write failed.""",index,mapping)
-          throw new Exception(msg)
-      
-        } else {
-      
-          /*
-           * Data preparation comprises the extraction of all common 
-           * fields, i.e. timestamp, site, user and group. The 'item' 
-           * field may specify a list of purchase items and has to be 
-           * processed differently.
-           */
-          val source = prepare(req.data)
-          /*
-           * The 'item' field specifies a comma-separated list
-           * of item (e.g.) product identifiers. Note, that every
-           * item is actually indexed individually. This is due to
-           * synergy effects with other data sources
-           */
-          val items = req.data(Names.ITEM_FIELD).split(",")
-          for  (item <- items) {
-            
-            /*
-             * Set or overwrite the 'item' field in the respective source
-             */
-            source.put(Names.ITEM_FIELD, item)
-            
-            /*
-             * Writing this source to the respective index throws an
-             * exception in case of an error; note, that the writer is
-             * automatically closed 
-             */
-            writer.write(index, mapping, source)
-          
-          }  
-        
-        }
-      
-      } catch {
-        
-        case e:Exception => {
-          log.error(e, e.getMessage())
-        }
-      
-      } finally {
-        
-        context.stop(self)
-
-      }
-    }
-    
-  }
-  
-  private def prepare(params:Map[String,String]):java.util.Map[String,Object] = {
+  override def prepareItem(params:Map[String,String]):java.util.Map[String,Object] = {
     
     val source = HashMap.empty[String,String]
     
